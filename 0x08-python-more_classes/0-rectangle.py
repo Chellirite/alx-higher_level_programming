90ba039f923e1cad3b50e6c0625780a5b933a139
Chellirite <davidadoga40@yahoo.com>
2024-01-04 16:20:06 -0800

    all good

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangular"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """A simple Rectangle class"""
+    pass
